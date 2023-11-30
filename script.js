c4929f7821c5da63f7b84c5fc03bce73d56888ac
Bastian Barthel <barthel.esq@gmail.com>
2023-11-30 13:25:13 +0100

    [#143] Add at method

--- script.js
+++ script.js
@@ -103,3 +103,10 @@ console.log(letters.join(' - '));
 // Reverse (mutates the original Array)
 arr2.reverse();
 console.log(arr2);
+
+// At (also works on Strings)
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.at(-1));
