0ef6877b77f4cb7ad644eef7ebaaabb7d0deb60f
Bastian Barthel <barthel.esq@gmail.com>
2023-11-30 13:57:14 +0100

    [#144] Add for-each loop

--- script.js
+++ script.js
@@ -71,8 +71,6 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -110,3 +108,40 @@ console.log(arr.at(0));
 
 console.log(arr[arr.length - 1]);
 console.log(arr.at(-1));
+
+// Looping over an Array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for...of Loop
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}€`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}€`);
+  }
+}
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// for-each Loop (doesn't break)
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}€`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}€`);
+  }
+});
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
